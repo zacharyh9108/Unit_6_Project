7c0563c131a6805d0e3c132d3183d96208c8b3fe
zacharyh9108 <zacharyhodge08@gmail.com>
2025-01-28 10:17:35 -0500

    Base code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String[] hi = getFileData("src/InputFile");
+        System.out.println(Arrays.toString(hi));
+    }
+
+    public static String[] getFileData(String fileName) {
+        String fileData = "";
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                fileData += s.nextLine() + "\n";
+            }
+            return fileData.split("\n");
+        } catch (FileNotFoundException e) {
+            return null;
+        }
     }
 }
