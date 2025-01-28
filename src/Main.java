7e26869d36850b2687156afbab5b9d63a47d93c8
Michelle <bths@ste.edu>
2025-01-28 10:53:48 -0500

    Processed and stored initial file data in format to be processed

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        String[] hi = getFileData("src/InputFile");
-        System.out.println(Arrays.toString(hi));
+        String[] hands = getFileData("src/InputFile");
+        System.out.println(Arrays.toString(hands));
     }
 
     public static String[] getFileData(String fileName) {
@@ -23,4 +22,35 @@ public class Main {
             return null;
         }
     }
+
+    public String partOne(String[] file) {
+        String[] cards = {"Ace", "King", "Queen", "Jack"};
+        ArrayList<String> hands = new ArrayList(file.length);
+        for (int i = 0; i < hands.size(); i++) {
+            String index = hands.get(i);
+            String[] w = index.split("\\|");
+        }
+        int fiveKind = 0;
+        int fourKind = 0;
+        int fullHouse = 0;
+        int threeKind = 0;
+        int twoPair = 0;
+        int pair = 0;
+        int highCard = 0;
+        //Map<cards, hands> map = new HashMap<>();
+        return "\n" +
+                "Number of five of a kind hands: " + fiveKind + "\n" +
+                "Number of full house hands: " + fourKind + "\n" +
+                "Number of four of a kind hands: " + fullHouse + "\n" +
+                "Number of three of a kind hands: " + threeKind + "\n" +
+                "Number of two pair hands: " + twoPair + "\n" +
+                "Number of one pair hands: " + pair + "\n" +
+                "Number of high card hands: " + highCard;
+
+    }
+
+    public String partTwo (String[] args){
+        return " ";
+    }
+
 }
